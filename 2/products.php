ecf64da3b03a65d10978b639895841c150d0763b
Jim Robinson <jimbo2150@gmail.com>
2025-12-02 18:31:52 -0500

    - Day 2 Part 2 complete

--- 2/products.php
+++ 2/products.php
@@ -7,7 +7,11 @@ $p1_valid_ids = 0;
 $p1_invalid_ids = 0;
 $p1_invalid_sum = 0;
 
-function is_valid(string $value) {
+$p2_valid_ids = 0;
+$p2_invalid_ids = 0;
+$p2_invalid_sum = 0;
+
+function p1_is_valid(string $value) {
 	$len = strlen($value);
 	$isEven = $len % 2 == 0;
 	// If value length is not even, it's valid
@@ -23,6 +27,23 @@ function is_valid(string $value) {
 	return true;
 }
 
+function p2_is_valid(string $value) {
+	$len = strlen($value);
+	if($len < 2) {
+		return true;
+	}
+	foreach(range($len - 1, 1) as $str_len) {
+		if($len % $str_len != 0) {
+			continue;
+		}
+		$split = str_split($value, $str_len);
+		if(count(array_unique($split)) == 1) {
+			return false;
+		}
+	}
+	return true;
+}
+
 while (($row = fgetcsv($file_handle, null, ",")) !== FALSE) {
 	foreach($row as $entry) {
 		$range = explode('-', $entry);
@@ -30,12 +51,18 @@ while (($row = fgetcsv($file_handle, null, ",")) !== FALSE) {
 			// Remove leading zeroes
 			$value = ltrim($value, '0');
 			// Check validity
-			if(is_valid($value)) {
+			if(p1_is_valid($value)) {
 				$p1_valid_ids += 1;
-				continue;
+			} else {
+				$p1_invalid_ids += 1;
+				$p1_invalid_sum += intval($value);
 			}
-			$p1_invalid_ids += 1;
-			$p1_invalid_sum += intval($value);
+			if(p2_is_valid($value)) {
+				$p2_valid_ids += 1;
+			} else {
+				$p2_invalid_ids += 1;
+				$p2_invalid_sum += intval($value);
+			}
 		}
 	}
 }
@@ -43,3 +70,7 @@ while (($row = fgetcsv($file_handle, null, ",")) !== FALSE) {
 echo 'Part 1: Valid ids: ', $p1_valid_ids, PHP_EOL;
 echo 'Part 1: Invalid ids: ', $p1_invalid_ids, PHP_EOL;
 echo 'Part 1: Invalid sum: ', $p1_invalid_sum, PHP_EOL;
+
+echo 'Part 2: Valid ids: ', $p2_valid_ids, PHP_EOL;
+echo 'Part 2: Invalid ids: ', $p2_invalid_ids, PHP_EOL;
+echo 'Part 2: Invalid sum: ', $p2_invalid_sum, PHP_EOL;
