c77b2eb3b15e5b1385c9b8d5dc193fabd23fd8c3
Jim Robinson <jimbo2150@gmail.com>
2025-12-03 20:12:32 -0500

    - Working on part 2

--- 3/joltage.php
+++ 3/joltage.php
@@ -7,26 +7,30 @@ $file_handle = fopen($input_file, 'r');
 
 $total_joltage = 0;
 
-$tens_selected_idx = -1;
-$ones_selected_idx = -1;
+$selected_idx = [];
+$needed_joltages = 1;
+
+$determine_joltages = function(string $digit) {
+	if($idx + 1 < $len) {
+		if($digit > $joltage[0]) {
+			$joltage[0] = $digit;
+			$tens_selected_idx = $idx;
+			$joltage[1] = 0;
+			return;
+		} 
+	}
+	if($digit > $joltage[1]) {
+		$joltage[1] = $digit;
+		$ones_selected_idx = $idx;
+	}
+};
 
 while (($row = fgets($file_handle)) !== FALSE) {
 	$digits = str_split(trim($row));
 	$len = count($digits);
 	$joltage = [0, 0];
 	foreach($digits as $idx => $digit) {
-		if($idx + 1 < $len) {
-			if($digit > $joltage[0]) {
-				$joltage[0] = $digit;
-				$tens_selected_idx = $idx;
-				$joltage[1] = 0;
-				continue;
-			} 
-		}
-		if($digit > $joltage[1]) {
-			$joltage[1] = $digit;
-			$ones_selected_idx = $idx;
-		}
+		$determine_joltages($digit);
 	}
 	$total_joltage += intval(implode('', $joltage));
 	$digits[$tens_selected_idx] = "\033[1m" . $digits[$tens_selected_idx] . "\033[0m";
